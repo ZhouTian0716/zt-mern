a95707160eda5ea2360abeec11ef261cad8595fa
ZhouTian0716 <egglord0716@gmail.com>
2021-08-07 13:32:18 +1000

    fixed?

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const db = require('./config/connection.js');
 
 
 const app = express();
-dotenv.config();
+
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
