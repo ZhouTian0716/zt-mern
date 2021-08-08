47596de39245d231499639b4ab377169b8447cef
ZhouTian0716 <egglord0716@gmail.com>
2021-08-09 01:31:12 +1000

    check

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -6,7 +6,7 @@ import decode from 'jwt-decode';
 import { LOGOUT } from '../../constants/actionTypes';
 
 import useStyles from './styles';
-import memories from '../../images/app.jpg';
+import logo from '../../images/app.jpg';
 
 const Navbar = () => {
     const classes = useStyles();
@@ -37,7 +37,7 @@ const Navbar = () => {
         <AppBar className={classes.appBar} position="static" color="inherit">
             <div className={classes.brandContainer}>
                 <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">M-NOTEs</Typography>
-                <img className={classes.image} src={memories} alt="icon" height="60" />
+                <img className={classes.image} src={logo} alt="icon" height="60" />
             </div>
             <Toolbar className={classes.toolbar}>
                 {user ? (
