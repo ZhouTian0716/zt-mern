f2c56d3f3beeba9be3587fa39ad72ce75cd57db6
ZhouTian0716 <egglord0716@gmail.com>
2021-08-09 12:53:37 +1000

    spotify redirect to deploy website

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ const spotifyApi = new SpotifyWebApi({
 
 
 // SPOTIFY CONSTANTS
-const redirectUri = 'http://localhost:3000';
+const redirectUri = 'https://zt-mern.herokuapp.com';
 const clientId = 'a64546f1c27541cca025fc19bce260c3';
 const AUTH_URL =`https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
 
